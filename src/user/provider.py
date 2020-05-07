1d3b7ff588c452e3edf8ece26d9c1c107c26efc1
Overrideveloper <bansowisdom@gmail.com>
2020-05-07 20:40:09 +0200

    refacor(core): Modify User Provider

--- src/user/provider.py
+++ src/user/provider.py
@@ -10,34 +10,33 @@ import src.db as db
 import time
 import bcrypt
 
-def signup(_req: UserReq) -> Response:
-    req = dict(_req)
-    users: List[User] = db.get(USERS_KEY) or []
+def signup(req: UserReq) -> Response:
+    users: List[dict] = db.get(USERS_KEY) or []
     
     if users:
         return Response(data = None, code = 403, message="This is a one-user system and a user already exists.")
     else:
-        user = dict(User(id=randomInt(), email=req["email"], password=bcrypt.hashpw(req["password"].encode(), bcrypt.gensalt())))
-        users.append(user)
+        user = User(id=randomInt(), email=req.email, password=bcrypt.hashpw(req.password.encode(), bcrypt.gensalt()))
+
+        users.append(dict(user))
         db.set(USERS_KEY, users)
         
-        token = TokenPayload(user = req["email"], expires = time.mktime((date.today() + timedelta(days=7)).timetuple()), randomizer = randomInt())
-        data = LoginResponse(email = user["email"], token = encodeJWT(token))
+        token = TokenPayload(user = user.email, expires = time.mktime((date.today() + timedelta(days=7)).timetuple()), randomizer = randomInt())
+        data = LoginResponse(email = user.email, token = encodeJWT(dict(token)))
 
         return Response(data = dict(data), code = 201, message="User signed up successfully")
 
-def login(_req: UserReq) -> Response:
-    req = dict(_req)
-    users: List[User] = db.get(USERS_KEY) or []
+def login(req: UserReq) -> Response:
+    users: List[dict] = db.get(USERS_KEY)
     
     if users:
         for _user in users:
-            if req["email"] == _user["email"] and bcrypt.checkpw(req["password"].encode(), _user["password"].encode()):
+            if req.email == _user["email"] and bcrypt.checkpw(req.password.encode(), _user["password"].encode()):
                 user = _user
         else:
             if user:
-                token = TokenPayload(user = req["email"], expires = time.mktime((date.today() + timedelta(days=7)).timetuple()), randomizer = randomInt())
-                data = LoginResponse(email = dict(user)["email"], token = encodeJWT(dict(token)))
+                token = TokenPayload(user = user["email"], expires = time.mktime((date.today() + timedelta(days=7)).timetuple()), randomizer = randomInt())
+                data = LoginResponse(email = user["email"], token = encodeJWT(dict(token)))
 
                 return Response(data = dict(data), code = 200, message="User logged in succesfully")
             else:
@@ -46,9 +45,9 @@ def login(_req: UserReq) -> Response:
         return Response(data = None, code = 404, message="User not found")
 
 def doesUserExist() -> Response:
-    users: List[User] = db.get(USERS_KEY) or []
+    users: List[dict] = db.get(USERS_KEY)
     
-    if users:
+    if users and len(users):
         return Response(data = True, code = 200, message="System user exists")
     else:
         return Response(data = False, code = 200, message="System user does not exist")
