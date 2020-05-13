243818830fa6e96df60f26d36e5bfa6d68122154
Overrideveloper <bansowisdom@gmail.com>
2020-05-13 09:57:42 +0200

    test(core): Add unit tests for EmailLog provider

--- src/modules/email_log/provider.py
+++ src/modules/email_log/provider.py
@@ -1,6 +1,6 @@
 from src.modules.email_log.models import EmailLog
 from src.constants import EMAIL_LOG_KEY
-from typing import List
+from typing import List, Union
 from src.utils import randomInt
 from datetime import date as _date
 from src.response_models import Response
@@ -24,21 +24,21 @@ class EmailLogProvider():
         logs.append(log.dict())
         self.db.set(EMAIL_LOG_KEY, logs)
 
-    def getTodaysLog(self) -> Response[EmailLog]:
+    def getTodaysLog(self) -> Union[Response[EmailLog], Response]:
         logs: List[dict] = self.db.get(EMAIL_LOG_KEY) or []
-        response: Response[EmailLog] = None
+        response: Union[Response[EmailLog], Response] = None
         log: dict = None
         
         today = _date.today()
         date = "{day}/{month}/{year}".format(day = today.day, month = today.month, year = today.year)
         
         for l in logs:
-            if l["date"] != date:
+            if l["date"] == date:
                 log = l
         else:
             if log:
                 response = Response[EmailLog](data = EmailLog(**log) , code = 200, message = "Log returned")
             else:
-                response = Response[EmailLog](data = None, code = 200, message = "Today's emails not sent yet")
+                response = Response(data = None, code = 200, message = "Today's emails not sent yet")
                 
         return response
---
+++ tests/unit/test_email_log_provider.py
@@ -0,0 +1,30 @@
+from src.db import IDatabase
+from tests.unit.mocks.db_mock import DatabaseMock
+from src.modules.email_log.provider import EmailLogProvider
+from src.modules.email_log.models import EmailLog
+from src.response_models import Response
+
+class TestEmailLogProvider:
+    dbMock: IDatabase = DatabaseMock()
+    emailLogProvider: EmailLogProvider = EmailLogProvider(dbMock)
+    
+    def test_creation(self):
+        assert self.emailLogProvider is not None
+        
+    def test_get_empty_log(self):
+        res = self.emailLogProvider.getTodaysLog()
+        
+        assert isinstance(res, Response)
+        assert res.code == 200
+        assert not res.data
+        
+    def test_add_todays_log_and_get_non_empty_log(self):
+        self.emailLogProvider.addTodayLog(5)
+        
+        res = self.emailLogProvider.getTodaysLog()
+        
+        assert isinstance(res, Response[EmailLog])
+        assert res.code == 200
+        assert res.data
+        assert isinstance(res.data, EmailLog)
+        assert res.data.count == 5
